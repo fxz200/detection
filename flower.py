d8806a96f0ca0149678e364336535a0ce9126a93
FXZ <tina0920284770@gmail.com>
2023-10-09 20:34:04 +0800

    8.33

--- flower.py
+++ flower.py
@@ -1,3 +1,5 @@
+#encoding = utf-8
+
 import pandas as pd
 import numpy as np
 import cv2
@@ -43,7 +45,7 @@ def Centroid(img):
     gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     ret,thresh = cv2.threshold(gray_image,127,255,0)
     M = cv2.moments(thresh)
-    #質心
+    
     cX = int(M["m10"] / M["m00"])
     cY = int(M["m01"] / M["m00"])
     
@@ -65,7 +67,7 @@ def Radius(img):
     gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     ret,thresh = cv2.threshold(gray_image,127,255,0)
     M = cv2.moments(thresh)
-    #質心
+  
     cX = int(M["m10"] / M["m00"])
     cY = int(M["m01"] / M["m00"])
     
@@ -82,20 +84,16 @@ def Cut(img):
     img1=cv2.imread((img))
     img2=cv2.imread((img))
     img3=cv2.imread((img))
-    #質心半徑
+  
     r=Radius(img)
-    ##遮蓋罩##
-    ##最大
-    #MASK1=np.zeros_like(img1)
+
     MASK1=np.zeros((img1.shape[0], img1.shape[1]), dtype=np.uint8) 
     cv2.circle(MASK1, (111,124), r, (255, 255, 255), -1) 
-    ##次之
-    #MASK2=np.zeros_like(img2)
+ 
     MASK2=np.zeros((img2.shape[0], img2.shape[1]), dtype=np.uint8) 
     cv2.circle(MASK2, (111,124), r*2//3, (255, 255, 255), -1)
 
-    ##最小 
-    #MASK3=np.zeros_like(img3)
+
     MASK3=np.zeros((img3.shape[0], img3.shape[1]), dtype=np.uint8) 
     cv2.circle(MASK3, (111,124), r//3, (255, 255, 255), -1)
     mask2 = cv2.subtract(MASK2, MASK3)
@@ -118,7 +116,7 @@ def Color_hist(cut1,cut2,cut3):
     img2_=cv2.cvtColor(img2,cv2.COLOR_BGR2HSV)
     img3_=cv2.cvtColor(img3,cv2.COLOR_BGR2HSV)
     
-    ##刪除純黑## 
+
     black=np.array([0,0,0])
     color_list_1_no_black=[]
     color_list_2_no_black=[]
@@ -179,7 +177,7 @@ def Compare_Hist(h1,h2):
     H1_array=np.float32(H1_array)
     H2_array=np.float32(H2_array)
     corr=(cv2.compareHist(H1_array,H2_array , method=0))#HISTCMP_CORREL
-    dis=(cv2.compareHist(H1_array,H2_array , method=cv2.HISTCMP_BHATTACHARYYA))#巴氏距離
+    dis=(cv2.compareHist(H1_array,H2_array , method=cv2.HISTCMP_BHATTACHARYYA))
     
     return corr ,dis
 
@@ -198,3 +196,122 @@ def To_Data(img):
         H3_remake.append(i*3)
     H=H1+H2_remake+H3_remake    
     return H
+
+import os
+import cv2
+import time
+import imutils
+import numpy as np
+sift = cv2.SIFT_create()
+
+
+def parse_sift_output(target_path):
+    """
+    Return:
+        kp: keypoint of hessian affine descriptor. location, orientation etc... OpenCV KeyPoint format. 
+        des: 128d uint8 np array
+    """
+    
+    # print(os.listdir("./sample"))
+    kp = []
+    des = []
+    with open(target_path, "r") as f:
+        lines = list(map(lambda x: x.strip(), f.readlines()))
+        num_descriptor = int(lines[1])
+        lines = lines[2:]
+        for i in range(num_descriptor):
+            # print(i, lines[i])
+            val = lines[i].split(" ")
+            x = float(val[0])
+            y = float(val[1])
+            a = float(val[2])
+            b = float(val[3])
+            c = float(val[4])
+            # TODO: generate ellipse shaped key point
+            # Refer: https://math.stackexchange.com/questions/1447730/drawing-ellipse-from-eigenvalue-eigenvector
+            # Refer: http://www.robots.ox.ac.uk/~vgg/research/affine/det_eval_files/display_features.m
+            # Refer: http://www.robots.ox.ac.uk/~vgg/research/affine/detectors.html
+            key_point = cv2.KeyPoint(x, y, 1)
+            sift_descriptor = np.array(list(map(lambda x: int(x), val[5:])), dtype=np.uint8)
+            kp.append(key_point)
+            des.append(sift_descriptor)
+        
+    
+    return kp, np.array(des)
+
+
+def resize(img):
+    h, w, _ = img.shape
+    if w > 500:
+        img = imutils.resize(img, width=500)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    return img
+
+def hessian_sift_extractor(path1, path2):
+    os.system('./hesaff_c++/hesaff {}'.format(path1))
+    os.system('./hesaff_c++/hesaff {}'.format(path2))
+
+    kp1, des1 = parse_sift_output(path1 + '.hesaff.sift')
+    kp2, des2 = parse_sift_output(path2 + '.hesaff.sift')
+    des1 = des1.astype('float32')
+    des2 = des2.astype('float32')
+    return kp1, des1, kp2, des2
+
+def sift_extractor(img1, img2):
+    kp1, des1 = sift.detectAndCompute(img1, None)
+    kp2, des2 = sift.detectAndCompute(img2, None)
+    return kp1, des1, kp2, des2
+
+
+def show_match(path1, path2):
+
+    img1 = cv2.imread(path1, 1)
+    img2 = cv2.imread(path2, 1)
+    img1_gray  = resize(img1.copy())
+
+    img2_gray  = resize(img2.copy())
+    
+    kp1, des1, kp2, des2 = hessian_sift_extractor(path1, path2)
+    # kp1, des1, kp2, des2 = sift_extractor(img1_gray, img2_gray)
+
+    #### FLANN
+    FLANN_INDEX_KDTREE = 0
+    index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
+    search_params = dict(checks = 50) 
+
+
+    begin = time.time()
+    flann = cv2.FlannBasedMatcher(index_params, search_params)
+    matches = flann.knnMatch(des1, des2, k=2)
+
+    # Need to draw only good matches, so create a mask
+    matchesMask = [[0, 0] for i in range(len(matches))]
+    
+
+    del_same = []
+    mat = 0
+    for i, (m, n) in enumerate(matches):
+        # ratio test as per Lowe's paper
+        if m.distance < 0.6 * n.distance:
+            mat += 1
+            matchesMask[i] = [1, 0]
+            del_same.append(m.trainIdx)
+    count = len(set(del_same))
+    print('time嚙瘦{:.5f}, points嚙瘦{}'.format(time.time() - begin, count))
+
+
+    draw_params = dict(matchColor = (0, 255, 0),
+                       singlePointColor = (255, 0, 0),
+                       matchesMask = matchesMask,
+                       flags = 0)
+    img3 = cv2.drawMatchesKnn(img1_gray, kp1, img2_gray, kp2, matches, None, **draw_params)
+    # img3 = cv2.drawKeypoints(img1, kp1, img1,flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS) 
+
+    cv2.namedWindow('img',cv2.WINDOW_AUTOSIZE)
+    cv2.imshow('img',img3)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    # cv2.imwrite('test.jpg', img3)
+
+
+
